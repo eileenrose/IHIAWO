044f73a174a8ef9e7edb4d2b82d3fef9950130cd
Eileen <erabuenaflor@gmail.com>
2016-07-24 16:05:21 -0700

    connected buttons

--- project-wtd/main.py
+++ project-wtd/main.py
@@ -4,6 +4,13 @@
 from google.appengine.api import users
 import webapp2
 import jinja2
+import os
+
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_environment = jinja2.Environment(
+  loader=jinja2.FileSystemLoader(template_dir))
+
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
@@ -16,6 +23,8 @@ class MainHandler(webapp2.RequestHandler):
                         users.create_login_url('/'))
 
         self.response.out.write('<html><body>%s</body></html>' % greeting)
+        template = jinja_environment.get_template('startpage.html')
+        self.response.out.write(template.render())
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
