cb0434c1bf09d14239b96cf9770e047d88e6fbe7
Eileen <erabuenaflor@gmail.com>
2016-07-25 10:46:37 -0700

    added player.py and changed main.py

--- project-wtd/main.py
+++ project-wtd/main.py
@@ -5,6 +5,7 @@ from google.appengine.api import users
 import webapp2
 import jinja2
 import os
+from player import Player
 
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
@@ -26,17 +27,38 @@ class SignInHandler(webapp2.RequestHandler):
     def get(self):
         user = users.get_current_user()
         if user:
-            greeting = ('Welcome, %s! Click <a href="/game">"play"</a> to begin the game (<a href="%s">sign out</a>)' %
-                    (user.nickname(), users.create_logout_url('/')))
+            greeting = ('<a href="%s">Sign out</a>' %
+                    (users.create_logout_url('/')))
         else:
             greeting = ('<a href="%s">Sign in to be registered on the leaderboard</a>.' %
                         users.create_login_url('/'))
 
         self.response.out.write('<html><body>%s</body></html>' % greeting)
 
+class AddScoreHandler(webapp2.RequestHandler):
+    def get(self):
+        name = self.request.get('name')
+        user = users.get_current_user()
+        score = self.request.get('score')
+        player = Player(name=name, email=user.nickname(), score=score)
+        player.put()
+        message = '<ul><li>%s, %s</li></ul>' % (name, email)
+        self.response.write(message)
+
+ss
+class ScoreHandler(webapp2.RequestHandler):
+    def get(self):
+        query = Player.query().order(Player.score)
+        player_list = query.fetch()
+        for p in player_list:
+            self.response.write('<p>%s</p>' % p.name)
+            s.put()
+
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
     ('/game', GameHandler),
     ('/sign', SignInHandler),
+    ('/add', AddScoreHandler),
+    ('/scores', ScoreHandler),
 ], debug=True)
