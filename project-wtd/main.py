72039bfb343bd863aadece7b38f1ee8e675ebb5b
Eileen <erabuenaflor@gmail.com>
2016-07-25 09:27:51 -0700

    moved sign in

--- project-wtd/main.py
+++ project-wtd/main.py
@@ -14,15 +14,6 @@ jinja_environment = jinja2.Environment(
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        user = users.get_current_user()
-        if user:
-            greeting = ('Welcome, %s! Click "play" to begin the game (<a href="%s">sign out</a>)' %
-                    (user.nickname(), users.create_logout_url('/')))
-        else:
-            greeting = ('<a href="%s">Sign in to be registered on the leaderboard</a>.' %
-                        users.create_login_url('/'))
-
-        self.response.out.write('<html><body>%s</body></html>' % greeting)
         template = jinja_environment.get_template('startpage.html')
         self.response.out.write(template.render())
 
@@ -31,8 +22,21 @@ class GameHandler(webapp2.RequestHandler):
         templategame = jinja_environment.get_template('level1.html')
         self.response.out.write(templategame.render())
 
+class SignInHandler(webapp2.RequestHandler):
+    def get(self):
+        user = users.get_current_user()
+        if user:
+            greeting = ('Welcome, %s! Click <a href="/game">"play"</a> to begin the game (<a href="%s">sign out</a>)' %
+                    (user.nickname(), users.create_logout_url('/')))
+        else:
+            greeting = ('<a href="%s">Sign in to be registered on the leaderboard</a>.' %
+                        users.create_login_url('/'))
+
+        self.response.out.write('<html><body>%s</body></html>' % greeting)
+
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
     ('/game', GameHandler),
+    ('/sign', SignInHandler),
 ], debug=True)
