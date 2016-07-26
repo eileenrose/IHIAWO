c4aa5eb9151f21d2bad0f2e3af36595f22439701
Demo User <demouser@Jorge.local>
2016-07-26 09:08:00 -0700

    Got it to scroll

--- project-wtd/static/script.js
+++ project-wtd/static/script.js
@@ -3,14 +3,27 @@ var canvas = document.getElementById('canvas')
 var context = canvas.getContext("2d");
 
 
+var background = new Image();
+var background2 = new Image();
 var duckImage = new Image();
-duckImage.onload = function () {
-    context.drawImage(duckImage, 100, 100);
+background.onload = function(){
+  context.drawImage(background, 0, 0)
 }
+background2.onload = function(){
+  context.drawImage(background2, 1788, 0)
+}
+duckImage.onload = function () {
+    context.drawImage(duckImage, 100, 200);
+}
+background.src = "static/background.png";
+background2.src = "static/background.png";
 duckImage.src = "static/duck option one.png";
 
+
 var currentx = 100;
-var currenty = 100;
+var currenty = 200;
+var backgroundX = 0;
+var background2X = 1788;
 var rightPressed = false;
 var leftPressed = false;
 var upPressed = false; //set to false to begin with since the character shouldn't be moving until we call the function
@@ -23,21 +36,41 @@ function keyDownHandler(e) {
         rightPressed = true; //when the right key is pressed, the character will move
         console.log("rightPressed")
         context.clearRect(0,0,canvas.width,canvas.height)
-        currentx += 10;
+
+        backgroundX -= 35;
+        background2X -= 35;
+
+        if (currentx < 400){
+          currentx += 35;
+        }
+        if (background2X < 0){
+          backgroundX = -35;
+          background2X = 1788 - 35;
+        }
+        context.drawImage(background, backgroundX, 0)
+        context.drawImage(background2, background2X, 0)
         context.drawImage(duckImage, currentx,currenty);
     }
     else if(e.keyCode == 37) {
         leftPressed = true;
         console.log("leftPressed")
         context.clearRect(0,0,canvas.width,canvas.height)
-        currentx -= 10;
+        context.drawImage(background, backgroundX, 0)
+        context.drawImage(background2, background2X, 0)
+        if (currentx > 0){
+          currentx -= 35;
+        }
         context.drawImage(duckImage, currentx,currenty);
     }
     else if(e.keyCode == 38) {
         upPressed = true;
         console.log("upPressed")
         context.clearRect(0,0,canvas.width,canvas.height)
-        currenty -= 10;
+        context.drawImage(background, backgroundX, 0)
+        context.drawImage(background2, background2X, 0)
+        if (currenty > 0){
+          currenty -= 35;
+        }
         context.drawImage(duckImage, currentx,currenty);
         duckImage.velX +=10;
     }
@@ -45,7 +78,11 @@ function keyDownHandler(e) {
       downPressed = true;
       console.log("downPressed")
       context.clearRect(0,0,canvas.width,canvas.height)
-      currenty += 10;
+      context.drawImage(background, backgroundX, 0)
+      context.drawImage(background2, background2X, 0)
+      if (currenty < 200){
+        currenty += 35;
+      }
       context.drawImage(duckImage, currentx,currenty);
       duckImage.velX +=10;
     }
@@ -71,7 +108,14 @@ function scrollWrapper(x, y){
     wrapper.scrollLeft = y;
 }
 function duckLocation(){
-  currentx - 100
-  currenty - 200
+  var x = currentx - 100
+  var y = currenty - 100
+  if (x<0){
+    x=0
+  }
+  if (y<0){
+    y=0
+  }
+  scrollWrapper(x,y)
 }
 window.setTimeout(duckLocation, 100);
