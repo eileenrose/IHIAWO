6ac8b3657b2b45bd1d94c4f0f275b205d7ef0b41
Demo User <demouser@D-10-154-64-122.dhcp4.washington.edu>
2016-07-25 20:36:38 -0700

    Added scroll functionality

--- project-wtd/static/script.js
+++ project-wtd/static/script.js
@@ -1,31 +1,65 @@
 
-var currY = 0;
 var canvas = document.getElementById('canvas')
 var context = canvas.getContext("2d");
 
 
-function drawDuck() {
-  context.clearRect(0,0,canvas.width,canvas.height);
-  context.drawImage(img,0,currY);
+var duckImage = new Image();
+duckImage.onload = function () {
+    context.drawImage(duckImage, 100, 100);
 }
-
-var img = new Image();
 img.src = "static/duck option one.png";
-img.onload = function () {
-  drawDuck()
-}
+
+var currentx = 0;
+var rightPressed = false;
+var leftPressed = false;
+var upPressed = false; //set to false to begin with since the character shouldn't be moving until we call the function
 
 document.addEventListener("keydown", keyDownHandler, false); // the next two lines are calling the function. When the keys are pressed
-
+document.addEventListener("keyup", keyUpHandler, false); //when the keys aren't pressed
+}
 function keyDownHandler(e) {
-    if(e.keyCode == 38) { //up key
-        console.log("upPressed");
-        currY -= 10;
-        drawDuck();
+    if(e.keyCode == 39) {
+        rightPressed = true; //when the right key is pressed, the character will move
+        console.log("rightPressed")
+        context.clearRect(0,0,canvas.width,canvas.height)
+        currentx += 10;
+        context.drawImage(duckImage, currentx,0);
     }
-    else if(e.keyCode == 40) { //down key
-        console.log("downPressed");
-        currY += 10;
-        drawDuck();
+    else if(e.keyCode == 37) {
+        leftPressed = true;
+        console.log("leftPressed")
+        context.clearRect(0,0,canvas.width,canvas.height)
+        currentx -= 10;
+        context.drawImage(duckImage, currentx,0);
+    }
+    else if(e.keyCode = 35) {
+        upPressed = true;
+        console.log("upPressed")
+        //context.clearRect(0,0,canvas.width,canvas.height)
+        //current += 10;
+        //context.drawImage(img, currentx,0);
+        duckImage.velX +=10;
     }
 }
+function keyUpHandler(e) {
+    if(e.keyCode == 39) {
+        rightPressed = false;
+    }
+    else if(e.keyCode == 37) {
+        leftPressed = false;
+    }
+    else if (e.keyCode == 35) {
+        upPressed = false;
+    }
+}
+
+function scrollWrapper(x, y){
+    var wrapper = document.getElementById('wrapper');
+    wrapper.scrollTop = x;
+    wrapper.scrollLeft = y;
+}
+function duckLocation(){
+  currentx - 100
+  currenty - 200
+}
+window.setTimeout(duckLocation, 100);
